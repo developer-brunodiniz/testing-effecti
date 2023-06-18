4b522fb1f66a2123df499d3697494e0bb90f3e43
Bruno Diniz <brunodiniz.dev@outlook.com>
2023-06-18 13:13:27 -0300

    Criando rotas

--- api/routes/web.php
+++ api/routes/web.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Http\Controllers\EmpresasController;
+use App\Http\Controllers\LicitacoesController;
+use App\Http\Controllers\ModalidadesController;
+use App\Http\Controllers\StatusController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +20,31 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('status', [StatusController::class, 'index']);
+Route::put('status/atualizar/{id?}', [StatusController::class, 'put'])
+    ->where('id', '[0-9]+');
+Route::post('status/criar', [StatusController::class, 'post']);
+Route::match(['get', 'delete'], 'status/deletar/{id}', [StatusController::class, 'delete'])
+    ->where('id', '[0-9]+');
+
+Route::get('empresas', [EmpresasController::class, 'index']);
+Route::put('empresa/atualizar/{id?}', [EmpresasController::class, 'put'])
+    ->where('id', '[0-9]+');
+Route::post('empresa/criar', [EmpresasController::class, 'post']);
+Route::match(['get', 'delete'], 'empresa/deletar/{id}', [EmpresasController::class, 'delete'])
+    ->where('id', '[0-9]+');
+
+Route::get('modalidades', [ModalidadesController::class, 'index']);
+Route::put('modalidade/atualizar/{id?}', [ModalidadesController::class, 'put'])
+    ->where('id', '[0-9]+');
+Route::post('modalidade/criar', [ModalidadesController::class, 'post']);
+Route::match(['get', 'delete'], 'modalidade/deletar/{id}', [ModalidadesController::class, 'delete'])
+    ->where('id', '[0-9]+');
+
+Route::get('licitacoes', [LicitacoesController::class, 'index']);
+Route::put('licitacoes/atualizar/{id?}', [LicitacoesController::class, 'put'])
+    ->where('id', '[0-9]+');
+Route::post('licitacoes/criar', [LicitacoesController::class, 'post']);
+Route::match(['get', 'delete'], 'licitacoes/deletar/{id}', [LicitacoesController::class, 'delete'])
+    ->where('id', '[0-9]+');
